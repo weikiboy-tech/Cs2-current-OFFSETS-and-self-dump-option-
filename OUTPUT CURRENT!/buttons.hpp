8f122f570032996555fb2e3c21fbab7135ec1a6c
weikiboy-tech <austriangamer70@gmail.com>
2026-06-03 12:34:21 +0200

    chore: add yesterday snapshot as OUTPUT CURRENT!

---
+++ OUTPUT CURRENT!/buttons.hpp
@@ -0,0 +1,29 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2026-06-02 15:57:02.907938900 UTC
+
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+
+namespace cs2_dumper {
+    // Module: client.dll
+    namespace buttons {
+        constexpr std::ptrdiff_t attack = 0x2064A90;
+        constexpr std::ptrdiff_t attack2 = 0x2064B20;
+        constexpr std::ptrdiff_t back = 0x2064D60;
+        constexpr std::ptrdiff_t duck = 0x2065030;
+        constexpr std::ptrdiff_t forward = 0x2064CD0;
+        constexpr std::ptrdiff_t jump = 0x2064FA0;
+        constexpr std::ptrdiff_t left = 0x2064DF0;
+        constexpr std::ptrdiff_t lookatweapon = 0x2355150;
+        constexpr std::ptrdiff_t reload = 0x2064A00;
+        constexpr std::ptrdiff_t right = 0x2064E80;
+        constexpr std::ptrdiff_t showscores = 0x2355030;
+        constexpr std::ptrdiff_t sprint = 0x2064970;
+        constexpr std::ptrdiff_t turnleft = 0x2064BB0;
+        constexpr std::ptrdiff_t turnright = 0x2064C40;
+        constexpr std::ptrdiff_t use = 0x2064F10;
+        constexpr std::ptrdiff_t zoom = 0x23550C0;
+    }
+}
---
+++ OUTPUT CURRENT!/offsets.cs
@@ -0,0 +1,52 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2026-06-02 15:57:02.907938900 UTC
+
+namespace CS2Dumper.Offsets {
+    // Module: client.dll
+    public static class ClientDll {
+        public const nint dwCSGOInput = 0x2355230;
+        public const nint dwEntityList = 0x24E6590;
+        public const nint dwGameEntitySystem = 0x24E6590;
+        public const nint dwGameEntitySystem_highestEntityIndex = 0x2090;
+        public const nint dwGameRules = 0x2340158;
+        public const nint dwGlobalVars = 0x20606D0;
+        public const nint dwGlowManager = 0x233CF50;
+        public const nint dwLocalPlayerController = 0x231F700;
+        public const nint dwLocalPlayerPawn = 0x2340698;
+        public const nint dwPlantedC4 = 0x234EF88;
+        public const nint dwPrediction = 0x23405A0;
+        public const nint dwSensitivity = 0x233DA68;
+        public const nint dwSensitivity_sensitivity = 0x58;
+        public const nint dwViewAngles = 0x23558B8;
+        public const nint dwViewMatrix = 0x2345B30;
+        public const nint dwViewRender = 0x2345ED8;
+        public const nint dwWeaponC4 = 0x22BDD00;
+    }
+    // Module: engine2.dll
+    public static class Engine2Dll {
+        public const nint dwBuildNumber = 0x60CC74;
+        public const nint dwNetworkGameClient = 0x90A1A0;
+        public const nint dwNetworkGameClient_clientTickCount = 0x378;
+        public const nint dwNetworkGameClient_deltaTick = 0x24C;
+        public const nint dwNetworkGameClient_isBackgroundMap = 0x2C141F;
+        public const nint dwNetworkGameClient_localPlayer = 0xF8;
+        public const nint dwNetworkGameClient_maxClients = 0x240;
+        public const nint dwNetworkGameClient_serverTickCount = 0x24C;
+        public const nint dwNetworkGameClient_signOnState = 0x230;
+        public const nint dwWindowHeight = 0x90E5C4;
+        public const nint dwWindowWidth = 0x90E5C0;
+    }
+    // Module: inputsystem.dll
+    public static class InputsystemDll {
+        public const nint dwInputSystem = 0x42B50;
+    }
+    // Module: matchmaking.dll
+    public static class MatchmakingDll {
+        public const nint dwGameTypes = 0x1B0F80;
+    }
+    // Module: soundsystem.dll
+    public static class SoundsystemDll {
+        public const nint dwSoundSystem = 0x512360;
+        public const nint dwSoundSystem_engineViewData = 0x7C;
+    }
+}
